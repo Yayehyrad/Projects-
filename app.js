bcf382de9cab9b8c4d5cacb282a0cac475a63bcb
Yayehyrad <yayehyrad.sisay@gmail.com>
2023-05-19 00:20:43 -1200

    data

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const fs = require('fs')
+require("./resources/data.json")
 
+
+const tours = JSON.parse(fs.readFileSync("./resources/data.json"))
 const app = express();
-app.get("/" , (req , res)=>{
-    res.status(200).json({success:"success"})
+app.get("/api/v1/tours" , (req , res)=>{
+    res.status(200).json({status:"success" , data:{tours}})
 })
 
 app.listen(3000 , ()=>{
